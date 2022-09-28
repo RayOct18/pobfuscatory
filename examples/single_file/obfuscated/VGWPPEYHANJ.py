3580f82227e24f9a03a2bf2fa4e3c0267013d523
Ray <Ray_Xie@wistron.com>
2022-09-28 14:58:12 +0800

    docs: add singe file obfuscate example

---
+++ examples/single_file/obfuscated/VGWPPEYHANJ.py
@@ -0,0 +1,17 @@
+
+
+def VGWPPEYHANJ():
+    print("hello world")
+
+
+OVtlPPXKkEjzCjArbkGA = "aKVOXIEnTcNJ"
+fMnIYyKXs = "gZdbkpBqMJAFMKH75rBEx"
+BmXybSFIUvdQDSWfzWp = "b5sBDFmADCX3B9"
+mDZviKttgfADKJxN = "Bb5EjRky3jf4InCNDY84Re2SzSDpia"
+ERdiXZbZ = "lxX12H5MzRrPIV9UQYLC8"
+if __name__ == "__main__":
+    rLZtjW = "OcHU9kM1LVUoBST2PpB9rJoKk4"
+    ZwtfwhfZKQeRb = "IWoKfu1ehqRvCzt9Gl0o0256jOSpq"
+    RBqdBoaPe = "KF8Vmjet5fCzyIcukbJaln1bPolc"
+    BipnpB = "mNgqlxdL"
+    VGWPPEYHANJ()
